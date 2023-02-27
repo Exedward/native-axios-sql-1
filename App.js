668ae92a8f223f3fb9c7777cab8800aad7ec8930
Edu <eduardo.ex7@gmail.com>
2023-02-27 10:36:03 -0300

    Alt. 1

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, TextInput } from 'react-native';
+import api from './src/services/api';
 
 export default function App() {
+
+  const [frase, setFrase] = useState('')
+  const [valorInput, setValorInput] = useState('Ola')
+
+  async function consultar(){
+
+    const {data}  = await api.get('/')
+    setFrase(data.nome)
+    console.log(data.nome)
+    valorInput=null
+  }
+
+  async function setar(){
+    await api.post('/insert', {'nome': 'Eduardo'})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput 
+        style={styles.textEntrada}
+        placeholder='Frase aqui'
+        textAlign='center'
+        onChangeText={setValorInput}
+        editable={true}
+        value={valorInput}
+      />
+      <TouchableOpacity 
+        style={styles.touch}
+        onPress={consultar}
+      >
+      <Text style={styles.textButton}>CONSULTAR</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+        style={styles.touch}
+        onPress={setar}
+      >
+      <Text style={styles.textButton}>SETAR</Text>
+      </TouchableOpacity>
+      <Text style={styles.textTituloFrase}>Frase Configurada:</Text>
+      <Text style={styles.textFrase}>{frase}</Text>
     </View>
   );
 }
@@ -13,8 +50,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#aaa'
   },
+  touch:{
+    backgroundColor: 'red',
+    borderRadius: 7,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 20,
+  },
+  textButton:{
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  textFrase:{
+    fontSize: 17,
+    marginTop: 50,
+  },
+  textEntrada:{
+    width: '80%',
+    borderWidth: 1,
+    fontSize: 15,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 7,
+    marginBottom: 30,
+  },
+  textTituloFrase:{
+    marginTop: 30,
+    fontWeight: 'bold',
+    fontSize: 18
+  }
 });
